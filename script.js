29291a2d0f919596c2a2811f7500bf912468ef56
Maajid Al Rasheeth <rasheethalmajith786@gmail.com>
2022-11-10 04:12:04 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputEl = document.getElementById('input-el');
+const convertBtn = document.getElementById('convert-btn');
+const meterEl = document.getElementById('meter-el');
+const feetEl = document.getElementById('feet-el');
+const literEl = document.getElementById('liter-el');
+const gallonEl = document.getElementById('gallon-el');
+const kiloEl = document.getElementById('kilo-el');
+const poundEl = document.getElementById('pound-el')
+
+
+convertBtn.addEventListener('click', function () {
+
+  meterEl.textContent = `${inputEl.value} Meters = ${(inputEl.value * 3.281).toFixed(3)} Feet`;
+  feetEl.textContent = `${inputEl.value} Feet = ${(inputEl.value * 0.3048).toFixed(3)} Meters`;
+  literEl.textContent = `${inputEl.value} Liters = ${(inputEl.value / 3.785
+  ).toFixed(3)} Gallons`;
+  gallonEl.textContent = `${inputEl.value} Gallons = ${(inputEl.value * 3.785).toFixed(3)} Liters`;
+  kiloEl.textContent = `${inputEl.value} Kilos = ${(inputEl.value * 2.205
+  ).toFixed(3)} Pounds`
+  poundEl.textContent = `${inputEl.value} Pounds = ${(inputEl.value / 2.205).toFixed(3)} Kilos`
+  inputEl.value = ''
+})
+
