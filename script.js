91860dc74e3276ca44d6f5ffb012704e3cc58a4d
Maajid Al Rasheeth <rasheethalmajith786@gmail.com>
2022-11-11 19:53:46 +0530

    updated script

--- script.js
+++ script.js
@@ -11,18 +11,21 @@ const dayEl = document.getElementById('day-el');
 
 
 convertBtn.addEventListener('click', function () {
-
-  meterEl.textContent = `${inputEl.value} Meters = ${(inputEl.value * 3.281).toFixed(3)} Feet`;
-  feetEl.textContent = `${inputEl.value} Feet = ${(inputEl.value * 0.3048).toFixed(3)} Meters`;
-  literEl.textContent = `${inputEl.value} Liters = ${(inputEl.value / 3.785
-  ).toFixed(3)} Gallons`;
-  gallonEl.textContent = `${inputEl.value} Gallons = ${(inputEl.value * 3.785).toFixed(3)} Liters`;
-  kiloEl.textContent = `${inputEl.value} Kilos = ${(inputEl.value * 2.205
-  ).toFixed(3)} Pounds`
-  poundEl.textContent = `${inputEl.value} Pounds = ${(inputEl.value / 2.205).toFixed(3)} Kilos`
-  hourEl.textContent = `${inputEl.value} Hours  = ${(inputEl.value / 24).toFixed(1)} Days`;
-  dayEl.textContent = `${inputEl.value} Days = ${(inputEl.value * 24
-  ).toFixed(1)} Hours`
-  inputEl.value = ''
+  const input = parseInt(inputEl.value);
+  if (input != 0) {
+    meterEl.textContent = `${inputEl.value} Meters = ${(inputEl.value * 3.281).toFixed(3)} Feet`;
+    feetEl.textContent = `${inputEl.value} Feet = ${(inputEl.value * 0.3048).toFixed(3)} Meters`;
+    literEl.textContent = `${inputEl.value} Liters = ${(inputEl.value / 3.785
+    ).toFixed(3)} Gallons`;
+    gallonEl.textContent = `${inputEl.value} Gallons = ${(inputEl.value * 3.785).toFixed(3)} Liters`;
+    kiloEl.textContent = `${inputEl.value} Kilos = ${(inputEl.value * 2.205
+    ).toFixed(3)} Pounds`
+    poundEl.textContent = `${inputEl.value} Pounds = ${(inputEl.value / 2.205).toFixed(3)} Kilos`
+    hourEl.textContent = `${inputEl.value} Hours  = ${(inputEl.value / 24).toFixed(1)} Days`;
+    dayEl.textContent = `${inputEl.value} Days = ${(inputEl.value * 24
+    ).toFixed(1)} Hours`
+    inputEl.value = ''
+  } else {
+    alert('Are you fucking Dumb, 0 multiplies with num its 0, You idiot, Enter Fucking Number which is greater than 0')
+  }
 })
-
